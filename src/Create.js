882df69f85840e1030895b8c9db128a5b55ffc6a
nurkocar <fatmanurkocar61@gmail.com>
2021-03-30 02:48:48 +0100

    Handled Submit in Create page, used isPending state while fetching, redirected home page after submit

--- src/Create.js
+++ src/Create.js
@@ -1,42 +1,66 @@
 import { useState } from "react";
+import { useHistory } from "react-router";
 
 const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('Nur');
+    const [isPending, setIsPending] = useState(false)
+    const history = useHistory()
 
+    const handleSubmit = ((e) => {
+        e.preventDefault();
+        const blog = { title, body, author }
+        setIsPending(true);
+
+        fetch(('http://localhost:8000/blogs'), {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(blog)
+        }).then(() => {
+            console.log('New blog has been added')
+            setIsPending(false);
+            history.push('/')
+        })
+    })
 
     return (
         <div className='create'>
             <h2> Add a new blog </h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label htmlFor="">Blog Title</label>
                 <input
                     required
                     type="text"
-                    value={ title }
+                    value={title}
                     onChange={(e) => setTitle(e.target.value)}
                 />
                 <label htmlFor="">Blog Body</label>
                 <textarea
                     required
-                    value={ body }
+                    value={body}
                     onChange={(e) => setBody(e.target.value)}
                 />
                 <label htmlFor="">Blog Author</label>
-                <select 
-                    name="" 
+                <select
+                    name=""
                     id=""
-                    value = { author }
-                    onChange = {(e) => setAuthor(e.target.value)}
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
                 >
                     <option value="Nur">Nur</option>
                     <option value="Ihsan">Ihsan</option>
                 </select>
+                <button>{!isPending ? 'Add blog' : 'Adding...'}</button>
+                {/* {isPending && <button>Adding...</button>} */}
             </form>
+
+            {/* 
             <p>{title}</p>
             <p>{body}</p>
-            <p>{author}</p>
+            <p>{author}</p> */}
 
 
         </div>
