5e5c8c7722390b2b3616c24eb059fb72185848e0
nurkocar <fatmanurkocar61@gmail.com>
2021-04-04 09:21:04 +0100

    Added a delete button to Blog Detail page and handled it #1

--- src/BlogDetail.js
+++ src/BlogDetail.js
@@ -1,10 +1,19 @@
-import { useParams } from "react-router";
+import { useHistory, useParams } from "react-router";
 import useFetch from "./useFetch";
 
 const BlogDetail = () => {
     const { id } = useParams()
     const { data: blog, isPending, error } = useFetch(`http://localhost:8000/blogs/${id}`)
+    const history  = useHistory()
 
+    const handleDelete = () => {
+        fetch(`http://localhost:8000/blogs/${id}`, {
+            method: 'DELETE'
+        }).then(() => {
+            history.push('/')
+        })
+    }
+    
     return ( 
         <div className = 'blog-details'>
             { isPending && <div>Loading</div> }
@@ -14,6 +23,7 @@ const BlogDetail = () => {
                     <h2>{ blog.title }</h2>
                     <p>Written by { blog.author }</p>
                     <div>{ blog.body }</div>
+                    <button onClick = {handleDelete}>Delete</button>
                 </article>
             )}
 
